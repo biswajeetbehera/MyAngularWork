0b77b050bd500514ac729027ee34ac700dda8320
uday_ahuja <ahujaud@gmail.com>
2018-11-25 14:06:20 +0530

    Added Component for Food Cart Udemy Angular Project

--- FoodCart/src/app/app.module.ts
+++ FoodCart/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShopingListEditComponent } from './shoping-list/shoping-list-edit/shoping-list-edit.component';
+import { RecipeDetailsComponent } from './recipe-book/recipe-details/recipe-details.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeBookComponent,
+    ShopingListComponent,
+    ShopingListEditComponent,
+    RecipeDetailsComponent,
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
